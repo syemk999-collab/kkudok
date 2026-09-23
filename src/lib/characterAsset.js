83ada0fb2ec59fe3bf62de5a5fefc007edcf260f
syemk999-collab <syemk999@gmail.com>
2026-09-23 11:12:20 +0900

    chore: disable deleted default character asset

--- src/lib/characterAsset.js
+++ src/lib/characterAsset.js
@@ -2,7 +2,7 @@ import { Capacitor, registerPlugin } from "@capacitor/core";
 
 const NativeCharacterAsset = registerPlugin("CharacterAsset");
 
-export const DEFAULT_CHARACTER_SRC = "/assets/kkudok/cancel_guide_character.png";
+export const DEFAULT_CHARACTER_SRC = null;
 
 function normalizeAsset(result = {}) {
   const custom = result?.mode === "CUSTOM" && result?.uri;
